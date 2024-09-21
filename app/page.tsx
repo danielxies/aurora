3f25d22c3bc68df607afd1fcb3194e5f13bb4335
danielemporia <xie476@purdue.edu>
2024-09-21 00:13:42 -0400

    new compoennt

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+"use client"
+
 import SimpleButton from "@/components/custom/SimpleButton"
 import SimpleIconButton from "@/components/custom/SimpleIconButton";
 import SimpleGradientButton from "@/components/custom/SimpleGradientButton";
 import SimpleToggle from "@/components/custom/SimpleToggle";
+import SparklingButton from "@/components/custom/SimpleSparklingButton";
 
 import { ComponentGrid } from "@/components/custom/ComponentGrid";
 import { Zap } from 'lucide-react';
@@ -9,33 +12,44 @@ import { Zap } from 'lucide-react';
 export default function Home() {
 
   const dataArray = [
-    { 
-      component: <SimpleButton color="apollo"/>, 
-      title: "Lamp Button", 
-      description: "A glowing button with a satisfying click animation." 
+    {
+      component: <SparklingButton
+        buttonText="Am I sparkling?"
+        sparklesCount={10}
+        sparkleColors={{first: '#A07CFE', second: '#FE8FB5'}}
+        buttonColor="#1c1917"
+        bubbleHover={true}
+      />,
+      title: "Sparkling Button",
+      description: "Inspired by MagicUI, this button is pretty neat."
     },
-    { 
-      component: <SimpleGradientButton fromColor="#10b981" toColor="#be123c"/>, 
-      title: "Gradient Button", 
-      description: "Double the fun with a soft, smooth, gradient." 
+    {
+      component: <SimpleButton color="apollo" />,
+      title: "Lamp Button",
+      description: "A glowing button with a satisfying click animation."
     },
-    { 
-      component: <SimpleToggle color="amber" />, 
-      title: "Switch Button", 
-      description: "Good for turning off something. " 
+    {
+      component: <SimpleGradientButton fromColor="#10b981" toColor="#be123c" />,
+      title: "Gradient Button",
+      description: "Double the fun with a soft, smooth, gradient."
     },
-    { 
-      component: <SimpleIconButton icon={Zap} color="emerald" />, 
-      title: "Lamp Icon", 
-      description: "A glowing button that displays an icon." 
+    {
+      component: <SimpleToggle color="amber" />,
+      title: "Switch Button",
+      description: "Good for turning off something. "
+    },
+    {
+      component: <SimpleIconButton icon={Zap} color="emerald" />,
+      title: "Lamp Icon",
+      description: "A glowing button that displays an icon."
     },
   ]
-  
+
 
   return (
     <div className="min-h-screen overflow-y-auto" style={{ backgroundColor: 'tan' }}>
       <div className="w-full">
-        <ComponentGrid data={dataArray}/>
+        <ComponentGrid data={dataArray} />
       </div>
     </div>
   );
